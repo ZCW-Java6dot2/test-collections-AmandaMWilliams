bd2b0dc5af1167e9416965707f97a132a1cb9d79
AmandaMWilliams <73121365+AmandaMWilliams@users.noreply.github.com>
2020-11-22 16:54:47 -0500

    PriorityQueue test complete

--- gettestie/src/test/java/rocks/zipcode/PriorityQueueTest.java
+++ gettestie/src/test/java/rocks/zipcode/PriorityQueueTest.java
@@ -1,4 +1,23 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.PriorityQueue;
+
 public class PriorityQueueTest {
+    @Test
+    public void priorityQueueTest(){
+        //given
+        PriorityQueue<String> priorityQueue = new PriorityQueue<>();
+        String expected1 = "Turkey";
+        String expected2 = null;
+        //when
+        priorityQueue.add("Turkey");
+        String actual1 = priorityQueue.poll();
+        String actual2 = priorityQueue.peek();
+        //then
+        Assert.assertEquals(expected1, actual1);
+        Assert.assertEquals(expected2, actual2);
+    }
 }

dd748afa23079a172f996721b977b26d68cb7d02
AmandaMWilliams <73121365+AmandaMWilliams@users.noreply.github.com>
2020-11-22 15:54:18 -0500

    ArrayDeque test complete

--- gettestie/src/test/java/rocks/zipcode/ArrayDequeTest.java
+++ gettestie/src/test/java/rocks/zipcode/ArrayDequeTest.java
@@ -1,4 +1,23 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayDeque;
+
 public class ArrayDequeTest {
+    @Test
+    public void arrayDequeTest(){
+        //given
+        ArrayDeque<String> arrayDeque = new ArrayDeque<>();
+        String expected = "Cow";
+        //when
+        arrayDeque.add("Cow");
+        arrayDeque.add("Sheep");
+        arrayDeque.add("Horse");
+        arrayDeque.add("Pig");
+        String actual = arrayDeque.pop();
+        //then
+        Assert.assertEquals(expected, actual);
+    }
 }

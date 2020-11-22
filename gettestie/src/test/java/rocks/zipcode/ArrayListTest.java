0814f43d3960de57a2d074ba0415d69d7815af66
AmandaMWilliams <73121365+AmandaMWilliams@users.noreply.github.com>
2020-11-22 15:49:34 -0500

    ArrayList Test complete

--- gettestie/src/test/java/rocks/zipcode/ArrayListTest.java
+++ gettestie/src/test/java/rocks/zipcode/ArrayListTest.java
@@ -1,4 +1,25 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class ArrayListTest {
+
+    @Test
+    public void ArrayListTest(){
+        //given
+        ArrayList<String> arrayList = new ArrayList<>();
+        String expected = "Fish";
+        //when
+        arrayList.add("Dog");
+        arrayList.add("Cat");
+        arrayList.add("Fish");
+        arrayList.add("Bird");
+        String actual = arrayList.get(2);
+        //then
+        Assert.assertEquals(expected, actual);
+
+    }
 }

b28d1a73001262113ebc865261de42d92670068d
AmandaMWilliams <73121365+AmandaMWilliams@users.noreply.github.com>
2020-11-22 16:33:57 -0500

    Iterator test complete

--- gettestie/src/test/java/rocks/zipcode/IteratorTest.java
+++ gettestie/src/test/java/rocks/zipcode/IteratorTest.java
@@ -1,4 +1,25 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class IteratorTest {
+    @Test
+    public void iteratorTest(){
+        //given
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        Integer expected = 3;
+        //when
+        arrayList.add(5);
+        arrayList.add(4);
+        arrayList.add(3);
+        arrayList.add(5);
+        arrayList.add(4);
+        arrayList.add(3);
+        Integer actual = arrayList.lastIndexOf(5);
+        //then
+        Assert.assertEquals(expected, actual);
+    }
 }

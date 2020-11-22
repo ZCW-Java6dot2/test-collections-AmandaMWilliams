8724547eca9d5a5f685a65fee885a01f149e0756
AmandaMWilliams <73121365+AmandaMWilliams@users.noreply.github.com>
2020-11-22 15:59:12 -0500

    HashMap Tests complete

--- gettestie/src/test/java/rocks/zipcode/HashMapTest.java
+++ gettestie/src/test/java/rocks/zipcode/HashMapTest.java
@@ -1,4 +1,34 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+
 public class HashMapTest {
+    @Test
+    public void hashMapTest(){
+        //given
+        HashMap<Integer, String> hashMap = new HashMap<>();
+        //when
+        hashMap.put(1, "One");
+        hashMap.put(2, "Two");
+        hashMap.put(3, "Three");
+        boolean assertion = hashMap.containsKey(2);
+        //then
+        Assert.assertTrue(assertion);
+    }
+
+    @Test
+    public void hashMapTest2(){
+        //given
+        HashMap<Integer, String> hashMap = new HashMap<>();
+        //when
+        hashMap.put(1, "One");
+        hashMap.put(2, "Two");
+        hashMap.put(3, "Three");
+        boolean assertion = hashMap.containsKey(4);
+        //then
+        Assert.assertFalse(assertion);
+    }
 }

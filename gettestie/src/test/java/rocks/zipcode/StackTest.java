3df30a3b2b8640a2955af3f95c443e0d3c875154
AmandaMWilliams <73121365+AmandaMWilliams@users.noreply.github.com>
2020-11-22 16:59:23 -0500

    stack test complete

--- gettestie/src/test/java/rocks/zipcode/StackTest.java
+++ gettestie/src/test/java/rocks/zipcode/StackTest.java
@@ -1,4 +1,34 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Stack;
+
 public class StackTest {
+
+    @Test
+    public void stackTest(){
+        //given
+        Stack<Integer> stack = new Stack<>();
+        //when
+        boolean isStackEmpty = stack.empty();
+        //then
+        Assert.assertTrue(isStackEmpty);
+    }
+
+    @Test
+    public void stackTest2(){
+        //given
+        Stack<Integer> stack = new Stack<>();
+        Integer expected = 7;
+        //when
+        stack.push(1);
+        stack.push(7);
+        stack.push(45658);
+        stack.pop();
+        Integer actual = stack.peek();
+        //then
+        Assert.assertEquals(expected, actual);
+    }
 }

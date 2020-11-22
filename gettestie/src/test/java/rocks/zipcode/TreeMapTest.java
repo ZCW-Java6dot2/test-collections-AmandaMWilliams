e122f32d741a88395e298029fdb4caa9db9f48b7
AmandaMWilliams <73121365+AmandaMWilliams@users.noreply.github.com>
2020-11-22 17:05:54 -0500

    tree map completed

--- gettestie/src/test/java/rocks/zipcode/TreeMapTest.java
+++ gettestie/src/test/java/rocks/zipcode/TreeMapTest.java
@@ -1,4 +1,23 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.TreeMap;
+
 public class TreeMapTest {
+    @Test
+    public void treeMapTest(){
+        //given
+        TreeMap<Integer, String> treeMap = new TreeMap<>();
+        Integer expected = 3;
+        //when
+        treeMap.put(1, "Apple");
+        treeMap.put(2, "Banana");
+        treeMap.put(3, "Carrot");
+        Integer actual = treeMap.lastKey();
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
 }

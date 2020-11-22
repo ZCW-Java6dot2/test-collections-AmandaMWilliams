518ccd8365369de1fbae0101550b55d381cd87ff
AmandaMWilliams <73121365+AmandaMWilliams@users.noreply.github.com>
2020-11-22 17:09:57 -0500

    Treemap and treeSet completed

--- gettestie/src/test/java/rocks/zipcode/TreeMapTest.java
+++ gettestie/src/test/java/rocks/zipcode/TreeMapTest.java
@@ -10,11 +10,12 @@ public class TreeMapTest {
     public void treeMapTest(){
         //given
         TreeMap<Integer, String> treeMap = new TreeMap<>();
-        Integer expected = 3;
+        Integer expected = 4;
         //when
         treeMap.put(1, "Apple");
         treeMap.put(2, "Banana");
         treeMap.put(3, "Carrot");
+        treeMap.put(4, "Carrot");
         Integer actual = treeMap.lastKey();
         //then
         Assert.assertEquals(expected, actual);
--- gettestie/src/test/java/rocks/zipcode/TreeSetTest.java
+++ gettestie/src/test/java/rocks/zipcode/TreeSetTest.java
@@ -1,4 +1,24 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.TreeMap;
+import java.util.TreeSet;
+
 public class TreeSetTest {
+    @Test
+    public void treeSetTest(){
+        //given
+        TreeSet<String> treeSet = new TreeSet<>();
+        Integer expected = 3;
+        //when
+        treeSet.add("Apple");
+        treeSet.add("Banana");
+        treeSet.add("Carrot");
+        treeSet.add("Carrot");
+        Integer actual = treeSet.size();
+        //then
+        Assert.assertEquals(expected, actual);
+    }
 }

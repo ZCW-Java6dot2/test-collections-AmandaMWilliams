ddaabe246aa99080b246b52e5abf0434c08145ef
AmandaMWilliams <73121365+AmandaMWilliams@users.noreply.github.com>
2020-11-22 17:18:43 -0500

    completed all

--- gettestie/src/test/java/rocks/zipcode/VectorTest.java
+++ gettestie/src/test/java/rocks/zipcode/VectorTest.java
@@ -1,4 +1,24 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Vector;
+
 public class VectorTest {
+    @Test
+    public void vectorTest(){
+        //given
+        Vector<String> vector = new Vector();
+        String expected = "fruit";
+        //when
+        vector.add("cake");
+        vector.add("fruit");
+        vector.add("meats");
+        vector.add("some other food");
+        String actual = vector.elementAt(1);
+        //then
+        Assert.assertEquals(expected, actual);
+
+    }
 }

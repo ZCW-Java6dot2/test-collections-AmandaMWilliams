28a68432a18dd25ff8e7d88c43b034507ba8da90
AmandaMWilliams <73121365+AmandaMWilliams@users.noreply.github.com>
2020-11-22 16:10:25 -0500

    HashSetTest complete

--- gettestie/src/test/java/rocks/zipcode/HashSetTest.java
+++ gettestie/src/test/java/rocks/zipcode/HashSetTest.java
@@ -1,4 +1,24 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashSet;
+
 public class HashSetTest {
+    @Test
+    public void hashSetTest(){
+        //given
+        HashSet<String> hashSet = new HashSet<>();
+        Integer expectedSize = 4;
+        //when
+        hashSet.add("Red");
+        hashSet.add("Green");
+        hashSet.add("Red");
+        hashSet.add("Green");
+        Integer actualSize = hashSet.size();
+        //then
+        Assert.assertNotEquals(expectedSize, actualSize);
+    }
+
 }

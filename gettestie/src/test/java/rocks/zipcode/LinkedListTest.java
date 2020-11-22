ccd8e8050b333d46aa0a2cc1f2107dcd1c872a43
AmandaMWilliams <73121365+AmandaMWilliams@users.noreply.github.com>
2020-11-22 16:41:41 -0500

    linkedListTest complete

--- gettestie/src/test/java/rocks/zipcode/LinkedListTest.java
+++ gettestie/src/test/java/rocks/zipcode/LinkedListTest.java
@@ -1,4 +1,25 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.LinkedList;
+
 public class LinkedListTest {
+    @Test
+    public void linkedListTest(){
+        //given
+        LinkedList<String> linkedList = new LinkedList<>();
+        String expected = "sad";
+        //when
+        linkedList.add("happy");
+        linkedList.add("sad");
+        linkedList.add("confused");
+        linkedList.add("silly");
+        linkedList.pop();
+        String actual = linkedList.getFirst();
+        //then
+        Assert.assertEquals(expected, actual);
+
+    }
 }
